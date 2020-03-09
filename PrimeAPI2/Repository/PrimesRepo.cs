e65d6dacd8994de118e15af3d5e2393e93942cbe
Armandulis <Armandulis@gmail.com>
2020-03-09 22:49:26 +0100

    Creating ques

---
+++ PrimeAPI2/Repository/PrimesRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeAPI2.Repository
+{
+    public class PrimesRepo
+    {
+        public int GetBycountry(string country)
+        {
+            return 5;
+        }
+
+    }
+}
--- PrimeDistributor/Program.cs
+++ PrimeDistributor/Program.cs
@@ -6,7 +6,24 @@ namespace PrimeDistributor
     {
         static void Main(string[] args)
         {
+
+            string[] primesQueue = new string[2];
+            primesQueue[0] = "http://localhost:54774/";
+            primesQueue[1] = "http://localhost:54775/";
+
             Console.WriteLine("Hello World!");
+        }
+
+
+        public static void IsNumberAPrime()
+        {
+
+
+        }
+
+        public static void AddPrimes()
+        {
+
         }
     }
 }
